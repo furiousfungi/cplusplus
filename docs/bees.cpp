298b4d7e5ef13842474caaa753855c62e8c25cb7
furiousfungi <gravity.potentialenergy@gmail.com>
2025-12-29 19:01:55 +0000

    Update bees.cpp

--- docs/bees.cpp
+++ docs/bees.cpp
@@ -4,25 +4,18 @@
 using namespace std;
 
 struct Bee {
-	char name;
+	string name;
 	int level;
 	bool gifted;
 	bool mutation;
 };
 
 int main() {
-	map<int, string> dict;
 
-	dict[0] = "False";
-	dict[1] = "True";
-
-	Bee bubble;
-	bubble.level = 15;
-	bubble.gifted = 1;
-	bubble.mutation = 0;
-
-	cout << "Bubble is level: " << bubble.level <<
-		"\nGifted status: " << dict[bubble.gifted] <<
-		"\nMutation status:" << dict[bubble.mutation] <<
+	Bee bubble{"Bubble Bee", 20, true, false };
+	cout << bubble.name <<
+		" is level: " << bubble.level <<
+		"\nGifted status: " << bubble.gifted <<
+		"\nMutation status:" << bubble.mutation <<
 		endl;
 }
